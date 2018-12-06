c7797bdce1ba6b93d40935d04612bccea83b4c01
Will Harris <Harriswill22@yahoo.com>
2018-12-06 11:21:46 -0500

    makes button its own component

---
+++ src/StampButton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const StampButton =(props) =>{
+    return ( <button  onClick={props.handleClick}
+    className="my-button"> Click me </button>
+);
+
+};
+
+
+
+
+
+
+export default StampButton
